5a4cfa66c778f87836e80be1ed08f6fca794bbdf
Mika Rinne <mika.rinne@oracle.com>
2025-12-18 18:41:35 +0200

    Change container log retrieval to last 10 rows
    - Update default tail from 20 to 10 in server.js
    - Update frontend to request 10 rows instead of 20

--- public/js/main.js
+++ public/js/main.js
@@ -3693,7 +3693,7 @@ async function showContainerLogs(logOcid, containerName) {
         // Get log group ID from config if available
         const config = getConfiguration();
         const params = new URLSearchParams();
-        params.append('tail', '20');
+        params.append('tail', '10');
         if (config.logGroupId) {
             params.append('logGroupId', config.logGroupId);
         }
